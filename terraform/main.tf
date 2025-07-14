2d0b417f5bea08c51ceca3b99aaddbbe2a384dd2
zaid alsharif <zaidalshreef@gmail.com>
2025-07-14 22:25:25 +0300

    Remove zone specification from PostgreSQL flexible server resource in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,6 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   version                = "14"
   administrator_login    = var.postgres_admin_username
   administrator_password = var.postgres_admin_password
-  zone                   = "1"
 
   storage_mb = 32768
 
